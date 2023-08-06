bb72dc6de7428116e05a1750913201ee87e21a65
Ben Millwood <thebenmachine+git@gmail.com>
2023-08-06 17:51:26 +0100

    editable bio

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -18,12 +18,30 @@ type LoginStatus a
   | LoggedIn a
   | LoggingOut
 
+type alias ApiUser =
+  { id : String
+  , facebookId : Maybe String
+  , name : Maybe String
+  , bio : String
+  }
+
+decodeApiUser : Json.Decode.Decoder ApiUser
+decodeApiUser =
+  Json.Decode.map4
+    (\i f n b -> { id = i, facebookId = f, name = n, bio = b })
+    (Json.Decode.field "userId" Json.Decode.string)
+    (Json.Decode.field "facebookId" (Json.Decode.nullable Json.Decode.string))
+    (Json.Decode.field "name" (Json.Decode.nullable Json.Decode.string))
+    (Json.Decode.field "bio" Json.Decode.string)
+
 type alias Model =
   { errors : List String
   , facebookLoggedIn : LoginStatus { userId : String, accessToken : String }
   , apiLoggedIn : LoginStatus { userId : String }
   , facebookUsers : Dict String Ports.FacebookUser
+  , apiUsers : Dict String ApiUser
   , facebookFriends : Maybe (List Json.Decode.Value)
+  , myBio : String
   }
 
 type Msg
@@ -33,6 +51,9 @@ type Msg
   | StartFacebookLogout
   | CheckApiLogin
   | ApiLoginResult (LoginStatus { userId : String })
+  | ApiUsers (List ApiUser)
+  | EditBio String
+  | SubmitBio
 
 init : () -> (Model, Cmd Msg)
 init () =
@@ -40,7 +61,9 @@ init () =
     , facebookLoggedIn = Unknown
     , apiLoggedIn = Unknown
     , facebookUsers = Dict.empty
+    , apiUsers = Dict.empty
     , facebookFriends = Nothing
+    , myBio = ""
     }
   , checkApiLogin
   )
@@ -49,63 +72,126 @@ view : Model -> Browser.Document Msg
 view model =
   let
     viewError err = Html.li [] [Html.text err]
+    viewUser user isMe =
+      let
+        facebookUser =
+          user.facebookId
+          |> Maybe.andThen (\fbid -> Dict.get fbid model.facebookUsers)
+        name =
+          case facebookUser of
+            Nothing -> Maybe.withDefault "[unknown]" user.name
+            Just u -> u.name
+        picture = facebookUser |> Maybe.map .picture
+      in
+      Html.div
+        [ Attributes.style "display" "flex"
+        , Attributes.class "user"
+        ]
+        [ case picture of
+            Nothing -> Html.div [ Attributes.height 50, Attributes.width 50 ] []
+            Just p ->
+              Html.img
+                [ Attributes.src p.url
+                , Attributes.height p.height
+                , Attributes.width p.width
+                ]
+                []
+        , Html.div
+            []
+            [ Html.div [] [Html.text name]
+            , Html.div [Attributes.class "user-bio"] (
+                if isMe
+                then
+                  [ Html.input
+                      [ Attributes.type_ "text"
+                      , Attributes.placeholder "short bio"
+                      , Attributes.value model.myBio
+                      , Events.onInput EditBio
+                      ]
+                      []
+                  , let
+                      saved = user.bio == model.myBio
+                    in
+                    Html.button
+                      [ Events.onClick SubmitBio
+                      , Attributes.disabled saved
+                      ]
+                      [ Html.text (if saved then "Saved" else "Save") ]
+                  ]
+                else
+                  [ Html.text user.bio ]
+              )
+            ]
+        ]
   in
   { title = "Flexiprocity"
   , body =
-      [ Html.h1 [] [ Html.text "flexiprocity" ]
-      , Html.ul [] (List.map viewError model.errors)
-      , let
-          button disabled text =
-            Html.button
-              [ Events.onClick StartFacebookLogin
-              , Attributes.disabled disabled
-              , Attributes.class "facebook-button"
-              , Attributes.class "facebook-login"
-              ]
-              [ Html.text text ]
-          viewLoggedIn userId =
-            case Dict.get userId model.facebookUsers of
-              Nothing -> [ Html.text "Logged in with Facebook" ]
-              Just user ->
-                [ Html.text ("Logged in as " ++ user.shortName ++ " ") ]
-        in
-        case model.facebookLoggedIn of
-          Unknown -> Html.p [] [button True "Checking Facebook login status"]
-          NotLoggedIn -> Html.p [] [ button False "Login with Facebook" ]
-          LoggingIn -> Html.p [] [ button True "Logging in..." ]
-          LoggedIn { userId } ->
-            Html.p []
-              [ Html.span
-                  [ Attributes.class "facebook-button"
-                  , Attributes.class "facebook-logged-in"
-                  ]
-                  (viewLoggedIn userId)
-              , Html.button
-                  [ Attributes.class "facebook-button"
-                  , Attributes.class "facebook-logout"
-                  , Events.onClick StartFacebookLogout
-                  ]
-                  [ Html.text "Logout" ]
-              ]
-          LoggingOut ->
-            Html.p []
-              [ Html.button
-                  [ Attributes.class "facebook-button"
-                  , Attributes.class "facebook-logout"
-                  , Attributes.disabled True
-                  ]
-                  [ Html.text "Logging out..." ]
-              ]
-      , Html.p [] [
-          case model.facebookFriends of
-            Nothing -> Html.text "Don't know friends yet"
-            Just friends ->
-              [ "Read "
-              , String.fromInt (List.length friends)
-              , " friends from Facebook"
-              ] |> String.concat |> Html.text
+      [ [ Html.h1 [] [ Html.text "flexiprocity" ]
+        , Html.ul [] (List.map viewError model.errors)
+        , let
+            button disabled text =
+              Html.button
+                [ Events.onClick StartFacebookLogin
+                , Attributes.disabled disabled
+                , Attributes.class "facebook-button"
+                , Attributes.class "facebook-login"
+                ]
+                [ Html.text text ]
+            viewLoggedIn userId =
+              case Dict.get userId model.facebookUsers of
+                Nothing -> [ Html.text "Logged in with Facebook" ]
+                Just user ->
+                  [ Html.text ("Logged in as " ++ user.shortName ++ " ") ]
+          in
+          case model.facebookLoggedIn of
+            Unknown -> Html.p [] [button True "Checking Facebook login status"]
+            NotLoggedIn -> Html.p [] [ button False "Login with Facebook" ]
+            LoggingIn -> Html.p [] [ button True "Logging in..." ]
+            LoggedIn { userId } ->
+              Html.p []
+                [ Html.span
+                    [ Attributes.class "facebook-button"
+                    , Attributes.class "facebook-logged-in"
+                    ]
+                    (viewLoggedIn userId)
+                , Html.button
+                    [ Attributes.class "facebook-button"
+                    , Attributes.class "facebook-logout"
+                    , Events.onClick StartFacebookLogout
+                    ]
+                    [ Html.text "Logout" ]
+                ]
+            LoggingOut ->
+              Html.p []
+                [ Html.button
+                    [ Attributes.class "facebook-button"
+                    , Attributes.class "facebook-logout"
+                    , Attributes.disabled True
+                    ]
+                    [ Html.text "Logging out..." ]
+                ]
         ]
-      ]
+      , case model.apiLoggedIn of
+          LoggedIn { userId } ->
+            case Dict.get userId model.apiUsers of
+              Just u -> [viewUser u True]
+              Nothing -> []
+          _ -> []
+      , [ Html.p [] [
+            case model.facebookFriends of
+              Nothing -> Html.text "Fetching Facebook friends..."
+              Just friends ->
+                [ if List.isEmpty friends
+                  then "None"
+                  else String.fromInt (List.length friends)
+                , " of your Facebook friends use Flexiprocity"
+                , if List.isEmpty friends
+                  then " :("
+                  else ""
+                ] |> String.concat |> Html.text
+          ]
+        ]
+      ] |> List.concat
   }
 
 handleHttpResult : Result Http.Error Msg -> Msg
@@ -114,15 +200,30 @@ handleHttpResult r =
     Err e -> AddError (Debug.toString e)
     Ok msg -> msg
 
+graphQL
+  :  { query : String
+     , operationName : String
+     , variables : List (String, Json.Encode.Value)
+     , decodeResult : Json.Decode.Decoder Msg
+     }
+  -> Cmd Msg
+graphQL { query, operationName, variables, decodeResult } =
+  let
+    body =
+      [ ("query", Json.Encode.string query)
+      , ("operationName", Json.Encode.string operationName)
+      , ("variables", Json.Encode.object variables)
+      ] |> Json.Encode.object
+  in
+  Http.post
+    { url = "/graphql"
+    , body = Http.jsonBody body
+    , expect = Http.expectJson handleHttpResult decodeResult
+    }
+
 checkApiLogin : Cmd Msg
 checkApiLogin =
   let
-    query = "mutation L{getOrCreateUserId(input: {}) {userId}}"
-    body =
-      [ ("query", Json.Encode.string query)
-      , ("operationName", Json.Encode.string "L")
-      , ("variables", Json.Encode.object [])
-      ] |> Json.Encode.object
     decodeResult =
       Json.Decode.at
         ["data", "getOrCreateUserId", "userId"]
@@ -133,10 +234,11 @@ checkApiLogin =
           Just userId -> ApiLoginResult (LoggedIn { userId = userId })
       )
   in
-  Http.post
-    { url = "/graphql"
-    , body = Http.jsonBody body
-    , expect = Http.expectJson handleHttpResult decodeResult
+  graphQL
+    { query = "mutation L{getOrCreateUserId(input: {}) {userId}}"
+    , operationName = "L"
+    , variables = []
+    , decodeResult = decodeResult
     }
 
 apiLogin : { accessToken : String } -> Cmd Msg
@@ -216,7 +318,50 @@ update msg model =
     CheckApiLogin ->
       (model, checkApiLogin)
     ApiLoginResult newState ->
-      tryApiLogin { model | apiLoggedIn = newState }
+      let
+        (newModel, cmd) = tryApiLogin { model | apiLoggedIn = newState }
+        lookupMe userId =
+          graphQL
+            { query = "query Q($u:BigInt!){userProfiles(condition:{userId:$u}){nodes{userId facebookId name bio}}}"
+            , operationName = "Q"
+            , variables = [("u", Json.Encode.string userId)]
+            , decodeResult =
+                Json.Decode.at ["data", "userProfiles", "nodes"]
+                  (Json.Decode.list decodeApiUser)
+                |> Json.Decode.map ApiUsers
+            }
+      in
+      case newState of
+        LoggedIn { userId } ->
+          (newModel, Cmd.batch [cmd, lookupMe userId])
+        _ -> (newModel, cmd)
+    ApiUsers users ->
+      let
+        isMe otherId =
+          case model.apiLoggedIn of
+            LoggedIn { userId } -> userId == otherId
+            _ -> False
+        addUser user accModel =
+          { accModel
+          | apiUsers = Dict.insert user.id user accModel.apiUsers
+          , myBio = if isMe user.id then Debug.log "bio" user.bio else accModel.myBio
+          }
+      in
+      ( List.foldl addUser model users
+      , Cmd.none
+      )
+    EditBio bio -> ({ model | myBio = bio }, Cmd.none)
+    SubmitBio ->
+      ( model
+      , graphQL
+          { query = "mutation B($b:String!){updateMe(input:{bio:$b}){user{userId facebookId name bio}}}"
+          , operationName = "B"
+          , variables = [("b", Json.Encode.string model.myBio)]
+          , decodeResult =
+              Json.Decode.at ["data", "updateMe", "user"] decodeApiUser
+              |> Json.Decode.map (ApiUsers << List.singleton)
+          }
+      )
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.map FromJS Ports.subscriptions
--- schema/structure.sql
+++ schema/structure.sql
@@ -1,13 +1,22 @@
 GRANT CONNECT ON DATABASE flexiprocity TO api;
-CREATE TABLE users
-  ( user_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY
-  , facebook_id text UNIQUE NOT NULL
-  );
-GRANT SELECT ON users TO api;
 
 CREATE FUNCTION get_facebook_id() RETURNS text
   LANGUAGE sql SECURITY INVOKER STABLE PARALLEL RESTRICTED
 AS $$SELECT current_setting('jwt.claims.facebookUserId', true)$$;
+
+CREATE TYPE audience AS ENUM
+  ( 'self'
+  , 'everybody'
+  );
+
+CREATE TABLE users
+  ( user_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+  , facebook_id text UNIQUE
+  , name text
+  , bio text NOT NULL DEFAULT ''
+  , visible_to audience NOT NULL DEFAULT 'self'
+  , fbid_visible_to audience
+  );
 
 CREATE FUNCTION current_user_id() RETURNS bigint
   LANGUAGE sql SECURITY DEFINER STABLE PARALLEL RESTRICTED
@@ -15,8 +24,34 @@ CREATE FUNCTION current_user_id() RETURNS bigint
     SELECT user_id FROM users
     WHERE facebook_id = get_facebook_id()
   $$;
-REVOKE EXECUTE ON FUNCTION current_user_id() FROM public;
-GRANT EXECUTE ON FUNCTION current_user_id() TO api;
+REVOKE EXECUTE ON FUNCTION current_user_id FROM public;
+GRANT EXECUTE ON FUNCTION current_user_id TO api;
+
+CREATE VIEW user_profiles AS
+  SELECT
+    user_id
+  , CASE
+      WHEN user_id = current_user_id() THEN facebook_id
+      WHEN fbid_visible_to <> 'everybody' THEN NULL
+      ELSE facebook_id
+    END AS facebook_id
+  , name
+  , bio
+  FROM users
+  WHERE visible_to = 'everybody' OR user_id = current_user_id();
+GRANT SELECT ON user_profiles TO api;
+
+CREATE FUNCTION update_me(bio text, visible_to audience) RETURNS users
+  LANGUAGE sql SECURITY DEFINER VOLATILE PARALLEL RESTRICTED
+  AS $$
+    UPDATE users
+    SET bio = COALESCE(update_me.bio, users.bio)
+    , visible_to = COALESCE(update_me.visible_to, users.visible_to)
+    WHERE user_id = current_user_id()
+    RETURNING *;
+  $$;
+REVOKE EXECUTE ON FUNCTION update_me FROM public;
+GRANT EXECUTE ON FUNCTION update_me TO api;
 
 CREATE FUNCTION get_or_create_user_id() RETURNS bigint
   LANGUAGE sql SECURITY DEFINER VOLATILE PARALLEL RESTRICTED
@@ -36,5 +71,5 @@ CREATE FUNCTION get_or_create_user_id() RETURNS bigint
     UNION ALL
     SELECT user_id FROM created
   $$;
-REVOKE EXECUTE ON FUNCTION get_or_create_user_id() FROM public;
-GRANT EXECUTE ON FUNCTION get_or_create_user_id() TO api;
+REVOKE EXECUTE ON FUNCTION get_or_create_user_id FROM public;
+GRANT EXECUTE ON FUNCTION get_or_create_user_id TO api;
