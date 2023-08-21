8f62303c1ea6434396a9a2b99961947883756ca1
Ben Millwood <thebenmachine+git@gmail.com>
2023-08-21 01:20:40 +0100

    dev/prod distinction

--- postgraphile/start.sh
+++ postgraphile/start.sh
@@ -1,25 +1,44 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-# "For development" per https://www.graphile.org/postgraphile/usage-cli/
+# per https://www.graphile.org/postgraphile/usage-cli/
+dev="\
+    --show-error-stack=json \
+    --watch \
+    --extended-errors hint,detail,errcode \
+    --owner-connection socket:/run/postgresql?db=flexiprocity&user=postgres \
+    --export-schema-graphql schema.graphql \
+    --graphiql / \
+    --enhance-graphiql \
+    --allow-explain \
+"
+
+prod="\
+    --retry-on-init-fail \
+    --extended-errors errcode \
+    --disable-query-log \
+"
+
+case "$1" in
+  dev)
+    opts=$dev;;
+  prod)
+    opts=$prod;;
+  *)
+    exit 1;;
+esac
+
 node_modules/.bin/postgraphile \
     --append-plugins @graphile-contrib/pg-simplify-inflector \
-    --watch \
     --dynamic-json \
     --no-setof-functions-contain-nulls \
     --no-ignore-rbac \
-    --show-error-stack=json \
-    --extended-errors hint,detail,errcode \
-    --export-schema-graphql schema.graphql \
-    --graphiql "/" \
-    --enhance-graphiql \
-    --allow-explain \
     --enable-query-batching \
     --legacy-relations omit \
-    --owner-connection "socket:/run/postgresql?db=flexiprocity&user=postgres" \
     --connection "socket:/run/postgresql?db=flexiprocity&user=api" \
     --jwt-secret "$(cat ../secrets/jwt/public-key.pem)" \
     --jwt-verify-algorithms RS256 \
     --jwt-verify-clock-tolerance 1 \
     --cors \
-    --schema public
+    --schema public \
+    $opts
