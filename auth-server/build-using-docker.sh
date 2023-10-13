a82b8ad94dd6208882902bcf1f85000838a3eabd
Ben Millwood <thebenmachine+git@gmail.com>
2023-10-13 17:04:59 +0100

    prod-tarball.sh

---
+++ auth-server/build-using-docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+if [ -z "$(docker container ls --all --quiet --filter name='^auth-server-build$')" ]
+then
+    mkdir -p ubuntu
+    docker run --name auth-server-build --mount type=bind,src=$PWD,dst=/mnt haskell:9.2 bash -c 'cd /mnt; cabal update; cabal install --only-dependencies'
+fi
+if [ -z "$(docker container ls --quiet --filter name='^auth-server-build$')" ]
+then
+    docker start auth-server-build
+fi
+docker exec auth-server-build bash -c 'cd /mnt; cabal install --install-method=copy --installdir=/mnt/ubuntu'
---
+++ prod-tarball.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+tmpdir=$(mktemp -d)
+trap 'rm -rfv "$tmpdir"' EXIT
+d="$tmpdir"/flexiprocity
+mkdir -p "$d"
+mkdir -p "$d"/auth-server
+cp auth-server/ubuntu/auth-server "$d"/auth-server/auth-server
+mkdir -p "$d"/frontend
+pushd frontend > /dev/null
+cp *.{css,html} driver.js "$d"/frontend
+elm make --optimize --output="$d"/elm.premin.js src/Main.elm
+uglifyjs "$d"/elm.premin.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output "$d"/elm.js
+rm "$d"/elm.premin.js
+popd > /dev/null
+mkdir -p "$d"/nginx/var/{log,run}
+cp nginx/{nginx.conf,start.sh} "$d"/nginx
+sed -i -re 's/(listen .*)57958;/\180;/' -e 's/(listen .*)57959 ssl;/\1443 ssl;/' "$d"/nginx/nginx.conf
+mkdir "$d"/postgraphile
+cp postgraphile/{package.json,postgraphile.tags.json5,start.sh} "$d"/postgraphile
+
+cat >> "$d"/start.sh <<'EOF'
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+tmux new-session \; \
+    split-window -h \; \
+    send-keys "source secrets/secrets.env" Enter \
+      "cd auth-server" Enter \
+      "./auth-server" Enter \; \
+    split-window -h \; \
+    send-keys "cd nginx" Enter "nginx -p $PWD/nginx -c nginx.conf" Enter \; \
+    split-window -h \; \
+    send-keys "cd postgraphile" Enter \
+      "npm install" Enter \
+      "./start.sh prod" Enter \; \
+    select-layout main-vertical \; \
+    select-pane -t 0
+EOF
+chmod +x "$d"/start.sh
+tar -C "$tmpdir" -czf flexiprocity.tar.gz flexiprocity
