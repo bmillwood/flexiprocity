bd876f473b42b0b28de8591e5558b1f18d622500
Ben Millwood <thebenmachine+git@gmail.com>
2023-11-06 04:26:33 +0000

    Sentry

--- nginx/start.sh
+++ nginx/start.sh
@@ -7,6 +7,8 @@ while sleep 1
 do
     rm -rf var
     mkdir -p var/{log,run}
+    source ../secrets/sentry.env
+    sed -re "s~SENTRY_URL~$SENTRY_URL~" nginx.conf.template > nginx.conf
     inotifywait --quiet -e modify nginx.conf &
     nginx -p "$PWD" -c nginx.conf -e var/log/error.log &
     wait %inotifywait
