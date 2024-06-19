bd3035110af307a0429da7822425e74110157210
Ben Millwood <thebenmachine+git@gmail.com>
2024-06-19 15:25:39 +0100

    auth-server + ghc-9.6

--- auth-server/build-using-docker.sh
+++ auth-server/build-using-docker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -x
 dockername=auth-server-build
+baseimage=haskell:9.6
 if [ -z "$(docker container ls --all --quiet --filter name="^$dockername"'$')" ]
 then
     mkdir -p ubuntu
-    docker run --detach --name "$dockername" --mount type=bind,src=$PWD,dst=/mnt haskell:9.4 bash -ec 'while true; do sleep 1e9; done'
+    docker run --detach --name "$dockername" --mount type=bind,src=$PWD,dst=/mnt "$baseimage" bash -ec 'while true; do sleep 1e9; done'
     docker exec "$dockername" bash -c 'cd /mnt; cabal update; cabal install --only-dependencies'
 fi
 if [ -z "$(docker container ls --quiet --filter name="^$dockername"'$')" ]
