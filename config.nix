a8d3b1fad855d20156c6974adf19c31e8f397776
Ben Millwood <thebenmachine+git@gmail.com>
2024-09-18 17:51:45 +0100

    try running postgraphile as a service

--- config.nix
+++ config.nix
@@ -5,6 +5,7 @@ let
   frontend = pkgs.callPackage ./frontend {
     inherit (cfg) gitRoot flexiprocitySubmodule;
   };
+  postgraphile = pkgs.callPackage ./postgraphile/postgraphile.nix {};
   inherit (lib) mkIf mkOption types;
 in
 {
@@ -27,15 +28,42 @@ in
   };
 
   config = mkIf cfg.enable {
-    systemd.services.authServer = {
-      description = "flexiprocity auth server";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        User = "api";
-        ExecStart = "${authServer}/bin/auth-server";
-        # Currently this dir has to be set up manually, which is a shame
-        Environment = "SECRETS_DIR=/home/api/secrets";
+    systemd.services = {
+      authServer = {
+        description = "flexiprocity auth server";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          User = "api";
+          ExecStart = "${authServer}/bin/auth-server";
+          # Currently this dir has to be set up manually, which is a shame
+          Environment = "SECRETS_DIR=/home/api/secrets";
+        };
+      };
+      postgraphile = {
+        description = "flexiprocity postgraphile";
+        after = [ "postgresql.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          User = "api";
+          ExecStart = lib.concatStringsSep " " [
+            "${postgraphile}/bin/postgraphile"
+            # "--append-plugins @graphile-contrib/pg-simplify-inflector"
+            "--dynamic-json"
+            "--no-setof-functions-contain-nulls"
+            "--no-ignore-rbac"
+            "--enable-query-batching"
+            "--legacy-relations omit"
+            "--connection "socket:/run/postgresql?db=flexiprocity&user=api""
+            "--jwt-secret \"$(cat /home/api/secrets/jwt/public-key.pem)\""
+            "--jwt-verify-algorithms RS256"
+            "--jwt-verify-clock-tolerance 1"
+            "--cors"
+            "--schema public"
+          ];
+          # Currently this dir has to be set up manually, which is a shame
+          Environment = "SECRETS_DIR=/home/api/secrets";
+        };
       };
     };
     users.users.api = {
