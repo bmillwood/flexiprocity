9a1db14445ec33a2836cefd74779932b7eaa4c6d
Ben Millwood <thebenmachine+git@gmail.com>
2024-09-19 17:21:20 +0100

    give up on postgraphile.nix for now

--- config.nix
+++ config.nix
@@ -5,7 +5,6 @@ let
   frontend = pkgs.callPackage ./frontend {
     inherit (cfg) gitRoot flexiprocitySubmodule;
   };
-  postgraphile = pkgs.callPackage ./postgraphile/postgraphile.nix {};
   inherit (lib) mkIf mkOption types;
 in
 {
@@ -44,25 +43,35 @@ in
         description = "flexiprocity postgraphile";
         after = [ "postgresql.service" ];
         wantedBy = [ "multi-user.target" ];
+        path = with pkgs; [
+          # I don't know why it needs bash
+          bash
+          nodePackages.nodejs
+          nodePackages.npm
+        ];
+        script = lib.concatStringsSep " " [
+          # -y: skip installation confirmation
+          "npm exec -y"
+          # seems like it should be implied by the command being run, but apparently not
+          "--package postgraphile"
+          "--package @graphile-contrib/pg-simplify-inflector"
+          "--"
+          "postgraphile"
+          "--append-plugins @graphile-contrib/pg-simplify-inflector"
+          "--dynamic-json"
+          "--no-setof-functions-contain-nulls"
+          "--no-ignore-rbac"
+          "--enable-query-batching"
+          "--legacy-relations omit"
+          "--connection 'socket:/run/postgresql?db=flexiprocity'"
+          "--jwt-secret \"$(cat /home/api/secrets/jwt/public-key.pem)\""
+          "--jwt-verify-algorithms RS256"
+          "--jwt-verify-clock-tolerance 1"
+          "--cors"
+          "--schema public"
+        ];
         serviceConfig = {
           User = "api";
-          ExecStart = lib.concatStringsSep " " [
-            "${postgraphile}/bin/postgraphile"
-            # "--append-plugins @graphile-contrib/pg-simplify-inflector"
-            "--dynamic-json"
-            "--no-setof-functions-contain-nulls"
-            "--no-ignore-rbac"
-            "--enable-query-batching"
-            "--legacy-relations omit"
-            "--connection "socket:/run/postgresql?db=flexiprocity&user=api""
-            "--jwt-secret \"$(cat /home/api/secrets/jwt/public-key.pem)\""
-            "--jwt-verify-algorithms RS256"
-            "--jwt-verify-clock-tolerance 1"
-            "--cors"
-            "--schema public"
-          ];
-          # Currently this dir has to be set up manually, which is a shame
-          Environment = "SECRETS_DIR=/home/api/secrets";
         };
       };
     };
