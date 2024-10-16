e7a237aec17c183e26555113539db2d14cad0f58
Ben Millwood <thebenmachine+git@gmail.com>
2024-10-16 16:22:19 +0100

    drop meddler from dev setup for now

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -3,8 +3,6 @@ tmux new-session \; \
      split-window -h \; \
      send-keys "cd auth-server" Enter "./build.sh" Enter \; \
      split-window \; \
-     send-keys "cd meddler" Enter "./build.sh" Enter \; \
-     split-window \; \
      send-keys "cd nginx" Enter "./start.sh" Enter \; \
      split-window \; \
      send-keys "cd postgraphile" Enter "./start.sh dev" Enter \; \
