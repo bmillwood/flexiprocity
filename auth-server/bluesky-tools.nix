251b587f419e7b467d3b1bcbefb47365b4266506
Ben Millwood <thebenmachine+git@gmail.com>
2025-05-26 13:38:11 +0100

    upgrade bluesky-tools
    bluesky-tools-0.6.0.2 has wider dependency bounds

--- auth-server/bluesky-tools.nix
+++ auth-server/bluesky-tools.nix
@@ -4,11 +4,11 @@
 }:
 mkDerivation {
   pname = "bluesky-tools";
-  version = "0.6.0.0";
+  version = "0.6.0.2";
   src = fetchgit {
     url = "https://github.com/bmillwood/bluesky-tools";
-    sha256 = "02wyrwdwq4y2c4r3y1bs6rflx1h1wvw4hxbp8342g1g6z1y89f2i";
-    rev = "2161365fa0970653d5a356828958b4bdc4cd78d8";
+    sha256 = "sha256-Du9mF7bYmZJjl/esMLxu7MF4qjSnbf00cTQyyhoG0c0=";
+    rev = "484ecc0335bcc49d5a0ca1c59299f2fbcce08d08";
     fetchSubmodules = true;
   };
   libraryHaskellDepends = [
