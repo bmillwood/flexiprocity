74ddcbb7b4dc59130a00c89c4cfcd3c8e469d384
Ben Millwood <thebenmachine+git@gmail.com>
2025-05-26 15:37:09 +0100

    unmark my custom oidc-client as broken

--- auth-server/default.nix
+++ auth-server/default.nix
@@ -5,12 +5,16 @@ in
 nixpkgs.haskellPackages.callPackage ./auth-server.nix {
   bluesky-tools =
     nixpkgs.haskellPackages.callPackage ./bluesky-tools.nix {};
-  oidc-client = haskell.lib.compose.overrideSrc {
-    src = fetchFromGitHub {
-      owner = "bmillwood";
-      repo = "haskell-oidc-client";
-      rev = "f9e6fe672b0e1e4cf89b29cad85ad07dcad364d4";
-      hash = "sha256-qFLBstUqgbSINa6RDPb/csfaQ0421i3Sm0/GiGqtrLI=";
-    };
-  } nixpkgs.haskellPackages.oidc-client;
+  oidc-client =
+    lib.pipe nixpkgs.haskellPackages.oidc-client [
+      (haskell.lib.compose.overrideSrc {
+        src = fetchFromGitHub {
+          owner = "bmillwood";
+          repo = "haskell-oidc-client";
+          rev = "f9e6fe672b0e1e4cf89b29cad85ad07dcad364d4";
+          hash = "sha256-qFLBstUqgbSINa6RDPb/csfaQ0421i3Sm0/GiGqtrLI=";
+        };
+      })
+      haskell.lib.markUnbroken
+    ];
 }
