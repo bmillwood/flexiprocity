e495e2536d3176385f74e7909103dedf1c7b88ea
Ben Millwood <thebenmachine+git@gmail.com>
2025-07-26 15:01:33 +0100

    nixos-unstable no longer sets PSQL

--- schema/config.nix
+++ schema/config.nix
@@ -43,7 +43,7 @@ in
       # I'd normally put --single-transaction on this psql, but you can't create databases
       # inside a transaction
       systemd.services.postgresql.postStart = lib.mkAfter ''
-        $PSQL --tuples-only --no-align -v ON_ERROR_STOP=1 <<EOF
+        psql --tuples-only --no-align -v ON_ERROR_STOP=1 <<EOF
           SELECT NOT EXISTS (SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'flexiprocity') AS need_db
           \gset
           \if :need_db
