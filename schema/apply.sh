067b5bb44f9e4082850810a609ba56a1b2dc3cb3
Ben Millwood <thebenmachine+git@gmail.com>
2026-04-26 15:18:40 +0100

    schema/apply.sh creates users

--- schema/apply.sh
+++ schema/apply.sh
@@ -5,6 +5,19 @@ export PGUSER=${PGUSER:-postgres}
 export PGDATABASE=${PGDATABASE:-flexiprocity}
 dropdb "$PGDATABASE" || true
 createdb "$PGDATABASE"
+
+for role in agent api meddler
+do
+  echo 'DO $$BEGIN'
+  # Oh no, bash substitution into sql strings. Don't pass anything weird.
+  cat <<EOF
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$role')
+    THEN CREATE ROLE $role WITH LOGIN;
+    END IF;
+EOF
+  echo 'END$$;'
+  echo "GRANT CONNECT ON DATABASE :DBNAME TO $role;"
+done | psql -v ON_ERROR_STOP=on
+
 psql -v ON_ERROR_STOP=on \
-     -c "GRANT CONNECT ON DATABASE flexiprocity TO api, meddler" \
-     -f structure.sql -f ../secrets/seeds.sql
+  -f structure.sql -f ../secrets/seeds.sql
