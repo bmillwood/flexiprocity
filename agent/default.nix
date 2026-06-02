cd3593c5d850ca7f9ee91d7135011da5b4cb6654
Ben Millwood <thebenmachine+git@gmail.com>
2026-06-02 02:44:32 +0100

    agent doesn't need bluesky-tools.nix either

--- agent/default.nix
+++ agent/default.nix
@@ -1,8 +1,2 @@
 { nixpkgs ? import <nixpkgs> {} }:
-let
-  inherit (nixpkgs) lib fetchFromGitHub haskell;
-in
-nixpkgs.haskellPackages.callPackage ./flexiprocity-agent.nix {
-  bluesky-tools =
-    nixpkgs.haskellPackages.callPackage ../auth-server/bluesky-tools.nix {};
-}
+nixpkgs.haskellPackages.callPackage ./flexiprocity-agent.nix {}
